12d6d92adc37aba8e7791b32ce1d3c92f3dd95b4
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-04 00:46:54 +0700

    Update startup-ubuntu.sh

--- startup-ubuntu.sh
+++ startup-ubuntu.sh
@@ -5,7 +5,7 @@ cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
-./build.sh
-cat << EOF >> ./.bashrc
-./ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8-01 -t 8
+/root/MineCoin-Phone/ccminer/build.sh
+cat << EOF >> /root/.bashrc
+/root/MineCoin-Phone/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8-01 -t 8
 EOF
