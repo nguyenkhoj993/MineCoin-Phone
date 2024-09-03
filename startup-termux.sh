4b840bc2c50c31ddeacbd98f51ad797a95009d71
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-04 00:47:21 +0700

    Update startup-termux.sh

--- startup-termux.sh
+++ startup-termux.sh
@@ -6,6 +6,6 @@ chmod +x ubuntu.sh
 /data/data/com.termux/files/home/ubuntu-in-termux/ubuntu.sh -y
 chmod +x ubuntu.sh
 /data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
-cat << EOF >> ../usr/etc/bash.bashrc
+cat << EOF >> /data/data/com.termux/files/usr/etc/bash.bashrc
 /data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
 EOF
