8eeee134bcb10fa4a4bec1a886af555d6995658d
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-04 01:32:34 +0700

    Update startup-ubuntu.sh

--- startup-ubuntu.sh
+++ startup-ubuntu.sh
@@ -3,5 +3,5 @@
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 cat << EOF >> /root/.bashrc
-/root/MineCoin-Phone/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8-01 -t 8
+/root/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8-01 -t 8
 EOF
