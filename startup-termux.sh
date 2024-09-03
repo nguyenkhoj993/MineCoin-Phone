f42112e1664fa43af5db4b67d659c20c1e5c915d
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-04 00:59:39 +0700

    Update startup-termux.sh

--- startup-termux.sh
+++ startup-termux.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
-chmod +x /data/data/com.termux/files/home/ubuntu-in-termux/ubuntu.sh
-/data/data/com.termux/files/home/ubuntu-in-termux/ubuntu.sh -y
-chmod +x /data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
-/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
-cat << EOF >> /data/data/com.termux/files/usr/etc/bash.bashrc
-/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
+cd ubuntu-in-termux
+chmod +x ubuntu.sh && ./ubuntu.sh -y && chmod +x ubuntu.sh && ./startubuntu.sh
+cat << EOF >> ../usr/etc/bash.bashrc
+./ubuntu-in-termux/startubuntu.sh
 EOF
