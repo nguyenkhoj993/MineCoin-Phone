2e21de4b49c0a1555b8d05a75485e4d58017530a
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-04 02:17:44 +0700

    Update fixt.sh

--- fixt.sh
+++ fixt.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat << EOF >> /data/data/com.termux/files/usr/etc/bash.bashrc
+sudo cat << EOF >> /data/data/com.termux/files/usr/etc/bash.bashrc
 /data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
 EOF
