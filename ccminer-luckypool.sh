fefc62b71a4e0a853d28979fd2603816aa0cde30
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-09-18 01:27:54 +0700

    Update ccminer-luckypool.sh

--- ccminer-luckypool.sh
+++ ccminer-luckypool.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cat << EOF >> /root/.bashrc
-/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg.note8-01 -p hybrid -t 8
+/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg.note8-001 -p hybrid -t 8
 EOF
