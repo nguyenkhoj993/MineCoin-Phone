d65be715badb154bb67a853bec9712b56f826565
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-03 22:56:14 +0700

    Delete termux-ubuntu.sh

--- termux-ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh" >> /data/data/com.termux/files/usr/etc/bash.bashrc
-git clone https://github.com/MFDGaming/ubuntu-in-termux.git
-cd ./ubuntu-in-termux && chmod +x ubuntu.sh && ./ubuntu.sh -y && chmod +x ubuntu.sh && ./startubuntu.sh
