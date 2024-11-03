712b0c22d189bbf79bba8a0ad1acbde617b2597e
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-03 23:00:20 +0700

    Delete ubuntu-ccminer.sh

--- ubuntu-ccminer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
