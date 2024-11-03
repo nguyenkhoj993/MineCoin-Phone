d2225ff5e3f07ca13bd57705201e89fd3bdd4375
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-04 01:04:49 +0700

    Update install-file.sh

--- install-file.sh
+++ install-file.sh
@@ -2,6 +2,6 @@
 #install ubuntu
 apt -y update && apt -y install git && apt -y upgrade && echo "/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh" >> /data/data/com.termux/files/usr/etc/bash.bashrc && git clone https://github.com/MFDGaming/ubuntu-in-termux.git && cd ./ubuntu-in-termux && chmod +x ubuntu.sh && ./ubuntu.sh -y && chmod +x ubuntu.sh && ./startubuntu.sh
 #install ccminer
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+apt -y update && apt -y install git && apt -y upgrade && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 #start config file
 git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git echo "/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh"
