bf63a6b5b33e450da46e26691b0b6ec47ab58cef
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-05 05:56:34 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,1 @@
-sh /root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
+sh /root/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
