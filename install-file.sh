f42b21ff8b45df6952ec3ab94a3ac974b8a186b0
khoinguyen993 <nguyenkhoinguyen1993@gmail.com>
2024-11-05 05:37:59 +0700

    add new command

--- install-file.sh
+++ install-file.sh
@@ -4,4 +4,4 @@ apt -y update && apt -y install git && apt -y upgrade && apt -y install proot-di
 #install ccminer
 apt -y update && apt -y install git && apt -y upgrade && apt -y install wget && apt -y install build-essential && apt -y install automake && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 #start config file
-git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git echo "/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh"
+cd .. && git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git && echo "/root/MineCoin-Phone/startubuntu.sh" >> /root/.bashrc
