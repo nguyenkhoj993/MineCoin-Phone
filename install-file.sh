871f31ba2ad8e5919b9eebe7b304d70c35d4cb6c
khoinguyen993 <nguyenkhoinguyen1993@gmail.com>
2024-11-05 05:54:25 +0700

    add new command

--- install-file.sh
+++ install-file.sh
@@ -4,4 +4,4 @@ apt -y update && apt -y install git && apt -y upgrade && apt -y install proot-di
 #install ccminer
 apt -y update && apt -y install git && apt -y upgrade && apt -y install wget && apt -y install build-essential && apt -y install automake && git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git && cd ./ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
 #start config file
-cd .. && git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git && chmod +x config.sh && echo "/root/MineCoin-Phone/startubuntu.sh" >> /root/.bashrc
+cd .. && git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git && chmod +x config.sh && echo "/root/MineCoin-Phone/config.sh" >> /root/.bashrc
