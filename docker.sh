63ea4fca199335853163b39accbf91488c2b38b5
khoinguyen993 <nguyenkhoinguyen1993@gmail.com>
2024-11-08 08:01:25 +0700

    add new command docker

--- docker.sh
+++ docker.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 # Thêm khóa GPG của Docker và repository Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt -y update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt -y update
 apt update
 
 # Cài đặt Docker
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 # Khởi động và kích hoạt Docker service
 sudo systemctl start docker
