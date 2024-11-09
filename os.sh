cdb70df9cea2e67c71c2e193ef736f6b2b6aea22
khoinguyen993 <nguyenkhoinguyen1993@gmail.com>
2024-11-09 13:44:00 +0700

    add newcommad

--- os.sh
+++ os.sh
@@ -1,2 +1,3 @@
-ubuntu
+/data/data/com.termux/files/home/ubuntu-in-termux/startubuntu.sh
+#ubuntu
 #docker run --network my_custom_network honeygain/honeygain -tou-accept -email nguyenkhoinguyen1993@gmail.com -pass Abc123!@# -device Note8
