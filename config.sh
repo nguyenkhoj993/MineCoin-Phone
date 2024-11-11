27fa1dbe8e4a0dc9c1c9a467ec6903ce11ddf72a
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-10 21:41:51 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p hybrid -t 8
-#/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
+#/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p hybrid -t 8
+/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
 #/root/xmrig/xmrig --coin=SAL -a randomx --url sg.salvium.herominers.com:1230 --user SaLvsAijsKJgVfqruwCtygG8fRMj8RnBJc8BdR2zvjnQFmjHQiMrRKMQrxjV39gw4oBkGGnqtFhW47GbfdHnQo1Zfpm3YL6sgWg.note8 -t 8
 #docker run --network my_custom_network honeygain/honeygain -tou-accept -email nguyenkhoinguyen1993@gmail.com -pass Abc123!@# -device Note8
