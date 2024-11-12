5f459f400958ce4d258b4590c0e8731658d33ed3
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-12 09:34:11 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-/root/ccminer/ccminer -a verus -o stratum+ssl://sg.vipor.net:5145 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
+/root/ccminer/ccminer -a verus -o stratum+tcp://sg.vipor.net:5045 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -t 8
 #/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p hybrid -t 8
 #/root/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8
 #/root/xmrig/xmrig --coin=SAL -a randomx --url sg.salvium.herominers.com:1230 --user SaLvsAijsKJgVfqruwCtygG8fRMj8RnBJc8BdR2zvjnQFmjHQiMrRKMQrxjV39gw4oBkGGnqtFhW47GbfdHnQo1Zfpm3YL6sgWg.note8 -t 8
