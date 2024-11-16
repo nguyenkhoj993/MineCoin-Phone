38a90ba8b512a88222e3472dcd53ea71c7b47d36
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-16 01:58:33 -0800

    Update install-file.sh

--- install-file.sh
+++ install-file.sh
@@ -5,3 +5,8 @@ apt -y update && apt -y upgrade && apt -y install proot-distro git wget vim && g
 apt -y update && apt -y upgrade && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata && apt -y install dialog apt-utils wget vim git build-essential automake libssl-dev libcurl4-openssl-dev cmake libuv1-dev libhwloc-dev && git clone https://github.com/nguyenkhoj993/MineCoin-Phone.git && chmod +x /root/MineCoin-Phone/config.sh && echo "/root/MineCoin-Phone/config.sh" >> /root/.bashrc && chmod +x /root/MineCoin-Phone/install-tool.sh && /root/MineCoin-Phone/install-tool.sh
 #switch command config.sh
 cd /root/MineCoin-Phone/ && git stash && git pull https://github.com/nguyenkhoj993/MineCoin-Phone.git && chmod +x /root/MineCoin-Phone/config.sh && cd ..
+#update command mining
+echo "/root/ccminer/ccminer -a verus -o stratum+ssl://sg.vipor.net:5145 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8" > /root/MineCoin-Phone/config.sh
+
+#/root/xmrig/xmrig --coin=SAL -a randomx --url sg.salvium.herominers.com:1230 --user SaLvsAijsKJgVfqruwCtygG8fRMj8RnBJc8BdR2zvjnQFmjHQiMrRKMQrxjV39gw4oBkGGnqtFhW47GbfdHnQo1Zfpm3YL6sgWg.note8 -t 8
+#docker run --network my_custom_network honeygain/honeygain -tou-accept -email nguyenkhoinguyen1993@gmail.com -pass Abc123!@# -device Note8
