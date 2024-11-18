0146f5be8894267655b878cb95228b8d4233e563
nguyenkhoj993 <65162253+nguyenkhoj993@users.noreply.github.com>
2024-11-17 17:14:53 -0800

    Update install-file.sh

--- install-file.sh
+++ install-file.sh
@@ -8,5 +8,9 @@ cd /root/MineCoin-Phone/ && git stash && git pull https://github.com/nguyenkhoj9
 #update command mining
 echo "/root/ccminer/ccminer -a verus -o stratum+ssl://sg.vipor.net:5145 -u RREbiCsQGmH6kQZf3p8dTATUzT1QxxSU88.note8 -p x -t 8" > /root/MineCoin-Phone/config.sh
 
+echo "/root/astrominer-V1.9.2.R5_aarch64_linux/astrominer -w dero1qyncf2x5uhm3hjareagyfxcxk0re947c9kf4u7y0acvks0jq8aafuqgk5fgad -r nodent2.cpumining.cloud:10100 -m 8 -p rpc" > /root/MineCoin-Phone/config.sh
+echo "/root/astrominer-V1.9.2.R5_aarch64_linux/astrominer -w dero1qyncf2x5uhm3hjareagyfxcxk0re947c9kf4u7y0acvks0jq8aafuqgk5fgad -r dero-node-orionure-sg.mysrv.cloud:10100 -m 8 -p rpc" > /root/MineCoin-Phone/config.sh
 #/root/xmrig/xmrig --coin=SAL -a randomx --url sg.salvium.herominers.com:1230 --user SaLvsAijsKJgVfqruwCtygG8fRMj8RnBJc8BdR2zvjnQFmjHQiMrRKMQrxjV39gw4oBkGGnqtFhW47GbfdHnQo1Zfpm3YL6sgWg.note8 -t 8
 #docker run --network my_custom_network honeygain/honeygain -tou-accept -email nguyenkhoinguyen1993@gmail.com -pass Abc123!@# -device Note8
+
+docker run -e HTTP_PROXY="http://bestproxyfpt1.camdvr.org:31890" -e HTTPS_PROXY="http://bestproxyfpt1.camdvr.org:31890" honeygain/honeygain -tou-accept -email "nguyenkhoinguyen1993@gmail.com" -pass "Abc123!@#" -device "Note8-01"
